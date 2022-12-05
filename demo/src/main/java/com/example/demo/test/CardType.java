99c5b6300e62223ac820f8b7bc4bef14850b5fd0
Hui Yi <huiyiprogram@outlook.com>
2022-12-05 17:59:38 +0800

    Added example polymorphism pattern
    Added an example online for polymorphism pattern to test out.

---
+++ demo/src/main/java/com/example/demo/test/CardType.java
@@ -0,0 +1,6 @@
+package com.example.demo.test;
+
+public enum CardType {
+    VISA,
+    MASTERCARD
+}
---
+++ demo/src/main/java/com/example/demo/test/VisaCardType.java
@@ -0,0 +1,26 @@
+package com.example.demo.test;
+
+public enum VisaCardType implements CardLogoType {
+
+    INFINITE("401"),
+    SIGNATURE("402"),
+    PLATINUM("403"),
+    GOLD("404"),
+    CLASSIC("405");
+
+    private String code;
+
+    VisaCardType(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public CardType getCardType() {
+        return CardType.VISA;
+    }
+}
